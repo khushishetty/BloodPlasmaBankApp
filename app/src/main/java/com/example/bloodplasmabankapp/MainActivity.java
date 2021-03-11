76f7735be18985425f12f5296fa6384d51cbd2aa
khushishetty <66870637+khushishetty@users.noreply.github.com>
2021-03-11 18:29:15 +0530

    Dashboard_ver_2

--- app/src/main/java/com/example/bloodplasmabankapp/MainActivity.java
+++ app/src/main/java/com/example/bloodplasmabankapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.bloodplasmabankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView c1,c2,c3,c4;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        c1 = (CardView)findViewById(R.id.bec_blood_donor_id);
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Become A Donor", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

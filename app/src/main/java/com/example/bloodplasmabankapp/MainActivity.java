4c93396e98d8c613850fde8c63cd10ac22b3e5db
khushishetty <66870637+khushishetty@users.noreply.github.com>
2021-05-09 14:38:01 +0530

    Week2

--- app/src/main/java/com/example/bloodplasmabankapp/MainActivity.java
+++ app/src/main/java/com/example/bloodplasmabankapp/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.bloodplasmabankapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
 
-    CardView c1,c2,c3,c4,c5,c6;
-    
+    CardView c1, c2, c3, c4, c5, c6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        c1 = (CardView)findViewById(R.id.bec_blood_donor_id);
-        c2 = (CardView)findViewById(R.id.search_blood_donor_id);
-        c3  =(CardView)findViewById(R.id.search_plasma_donor_id);
-        c4 = (CardView)findViewById(R.id.serach_nearby_hospitals);
-        c5 = (CardView)findViewById(R.id.view_requests_id);
-        c6 = (CardView)findViewById(R.id.requestid);
+
+        c1 = (CardView) findViewById(R.id.bec_blood_donor_id);
+        c2 = (CardView) findViewById(R.id.search_blood_donor_id);
+        c3 = (CardView) findViewById(R.id.search_plasma_donor_id);
+        c4 = (CardView) findViewById(R.id.serach_nearby_hospitals);
+        c5 = (CardView) findViewById(R.id.view_requests_id);
+        c6 = (CardView) findViewById(R.id.requestid);
 
         c1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this,DisplayBloodDonorsActivity.class);
+                Intent intent = new Intent(MainActivity.this, DisplayBloodDonorsActivity.class);
                 startActivity(intent);
 
             }
@@ -46,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         c3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,DisplayPlasmaDonorActivity.class);
+                Intent intent = new Intent(MainActivity.this, DisplayPlasmaDonorActivity.class);
                 startActivity(intent);
             }
         });
@@ -55,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(DetailedBloodDonorActivity.this, address.getText(), Toast.LENGTH_SHORT).show();
+                /*
                 String source = "Alangar,Moodbidri";
                 try{
                     Uri uri = Uri.parse("https://www.google.co.in/maps/search/Blood+Bank/"+source);
@@ -66,6 +81,17 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
 
                 }
+                */
+
+                if (ContextCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            1);
+                } else {
+                    getCurrentLocation();
+                }
+
+
             }
         });
 
@@ -84,5 +110,58 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(@NonNull LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                .removeLocationUpdates(this);
+                        if(locationResult !=null && locationResult.getLocations().size()>0){
+                            int latestLocationIndex  = locationResult.getLocations().size()-1;
+                            double latitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+
+                            Toast.makeText(MainActivity.this, String.format("Lat : "+latitude), Toast.LENGTH_SHORT).show();
+                            String uri = "geo:"+ latitude + "," + longitude +"?q=hospitals+near+me";
+                            startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri)));
+
+                        }
+
+                    }
+                }, Looper.getMainLooper());
+
+
     }
 }
